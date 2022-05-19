60c56f9da6dc95d935f9625bb4fbf1594306580e
Bratjia <hpdeme@gmail.com>
2022-05-18 21:36:48 -0400

    K

--- Actividad2/index2.js
+++ Actividad2/index2.js
@@ -1,12 +1,30 @@
 "use strict"
+ let notas= [];
 
 function enviarNombre() {
 var nombreAlumne=  document.getElementById("fname").value;
-
-document.getElementById("lblEstudiante").style.visibility= 'hidden';
-document.getElementById("fname").style.visibility= 'hidden';
-document.getElementById("btnEnviarnombre").style.visibility= 'hidden';
+document.getElementById("primero").style.visibility= 'hidden';
+document.getElementById("segundo").style.display = 'block';
 console.log(nombreAlumne);
+document.getElementById("lblEstudiante").innerHTML=  "Alumno:" + nombreAlumne;
+
+}
+
+function pedirNotas(){
+
+    notas.push(document.getElementById("txtNota1").value);
+    notas.push(document.getElementById("txtNota2").value);
+    notas.push(document.getElementById("txtNota3").value);
+    console.log(notas);
+    console.log(notaMinima);
+    let notaMinima= 70*60/100; //42
+    for (let i=0 ; i<notas.length; i++){
+       if( i > notaMinima)
+
+        console.log(notaMinima);
+
+    }
+
 
 
 }
